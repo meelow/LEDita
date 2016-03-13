43204cae4d52efcd6f451aa466915b38b2046f96
Meelow <milo.mittag@gmail.com>
2016-03-14 00:15:22 +0100

    renamed python directory to www (so it gets copied when uploading sketch)

---
+++ www/relayOSC.py
@@ -0,0 +1,52 @@
+from OSC import OSCServer,OSCClient, OSCMessage
+import sys
+from time import sleep
+import types
+
+													 
+#set up the OSC server to listen for the UI device 
+server = OSCServer( ("192.168.1.250", 10000) )
+
+# list of clients to relay the messages to:
+clients = [[OSCClient(), ("192.168.1.105", 8000)],
+		   [OSCClient(), ("192.168.1.107", 8000)], 
+		   [OSCClient(), ("192.168.1.199", 8000)]
+		  ]
+for client in clients:
+	client[0].connect( client[1] )
+
+
+# waiting for data to relay:
+def handle_timeout(self):
+	print ("Waiting for data...")
+
+server.handle_timeout = types.MethodType(handle_timeout, server)
+
+# this function will be called for every received message
+def relayMessageCallback(path, tags, args, source):
+	global faderLightFeedback
+#	print "--- received:"
+#	print "path="+str(path)
+#	print "tags="+str(tags)
+#	print "args="+str(args)
+#	print "source="+str(source)
+	for client in clients:
+		msg = OSCMessage(path)
+		msg.insert(0, args[0])
+		client[0].send(msg)
+		print "relaying to " + str(client[1]) + " : " + str(msg)
+		
+server.addMsgHandler( "/1/light/",relayMessageCallback) 
+server.addMsgHandler( "/1/fps/",relayMessageCallback) 
+server.addMsgHandler( "/1/rotary1/",relayMessageCallback) 
+for x in range(0,20):
+	server.addMsgHandler("/1/state/"+str(x)+"/1", relayMessageCallback)
+for x in range(0,20):
+	server.addMsgHandler("/1/palette/"+str(x)+"/1", relayMessageCallback)
+server.addMsgHandler("/1/xypad/", relayMessageCallback)
+server.addMsgHandler("/1/push1/", relayMessageCallback)
+	
+while True:
+	server.handle_request()
+
+server.close()
