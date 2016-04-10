d33c20b11a786431f86c9205ab51f273082b16b5
Meelow <milo.mittag@gmail.com>
2016-04-10 14:11:08 +0200

    Cleaned up relayOSC.py

--- www/relayOSC.py
+++ www/relayOSC.py
@@ -8,12 +8,13 @@ from tools import get_ip
 													 
 #set up the OSC server to listen for the UI device 
 server = OSCServer( (get_ip("wlan0"), 10000) )
+#server = OSCServer( ("192.168.1.250", 10000) )
 
 # list of clients to relay the messages to:
 clients = [
-#		   [OSCClient(), ("192.168.1.104", 8000)],
-		   [OSCClient(), ("192.168.1.105", 8000)],
-		   [OSCClient(), ("192.168.1.107", 8000)]
+		   [OSCClient(), ("192.168.1.104", 8000)],
+		   [OSCClient(), ("192.168.1.107", 8000)],
+		   [OSCClient(), ("192.168.1.199", 8000)]
 		   #, 
 #		   [OSCClient(), ("192.168.1.199", 8000)]
 		  ]
@@ -24,8 +25,6 @@ for client in clients:
 # waiting for data to relay:
 def handle_timeout(self):
 	print ("Waiting for data...")
-
-server.handle_timeout = types.MethodType(handle_timeout, server)
 
 # this function will be called for every received message
 def relayMessageCallback(path, tags, args, source):
@@ -41,16 +40,22 @@ def relayMessageCallback(path, tags, args, source):
 		client[0].send(msg)
 		print "relaying to " + str(client[1]) + " : " + str(msg)
 		
-server.addMsgHandler( "/1/light/",relayMessageCallback) 
-server.addMsgHandler( "/1/fps/",relayMessageCallback) 
-server.addMsgHandler( "/1/rotary1/",relayMessageCallback) 
-for x in range(0,20):
-	server.addMsgHandler("/1/state/"+str(x)+"/1", relayMessageCallback)
-for x in range(0,20):
-	server.addMsgHandler("/1/palette/"+str(x)+"/1", relayMessageCallback)
-server.addMsgHandler("/1/xypad/", relayMessageCallback)
-server.addMsgHandler("/1/push1/", relayMessageCallback)
+
+# setting up all call backs for the 		
+def setupCallbacks(client_list):
+	server.addMsgHandler( "/1/light/",relayMessageCallback) 
+	server.addMsgHandler( "/1/fps/",relayMessageCallback) 
+	server.addMsgHandler( "/1/rotary1/",relayMessageCallback) 
+	for x in range(0,20):
+		server.addMsgHandler("/1/state/"+str(x)+"/1", relayMessageCallback)
+	for x in range(0,20):
+		server.addMsgHandler("/1/palette/"+str(x)+"/1", relayMessageCallback)
+	server.addMsgHandler("/1/xypad/", relayMessageCallback)
+	server.addMsgHandler("/1/push1/", relayMessageCallback)
 	
+# script code itself:
+setupCallbacks(clients)
+server.handle_timeout = types.MethodType(handle_timeout, server)	
 while True:
 	server.handle_request()
 
